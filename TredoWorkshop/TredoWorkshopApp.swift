f855f91e66b001f1dcd56de60868f2d8de449f95
Izzatilla <izzatilla.sapayevgmail.com@MacBook-Pro-Izzatilla-2.local>
2021-03-20 14:59:55 +0600

    dasd

---
+++ TredoWorkshop/TredoWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TredoWorkshopApp.swift
+//  TredoWorkshop
+//
+//  Created by Izzatilla on 20.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TredoWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
